7017f0f5600bce5dc154912dbd5231b7970c1514
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-07-07 18:27:28 -0400

    Only show proposals with Display=y

--- main.js
+++ main.js
@@ -7,13 +7,28 @@ Tabletop.init({
 });
 
 function processData(data, tabletop) {
+  if (!data[0])
+    return;
+
+  var skipColumns = ['Timestamp', 'Display'];
+
   for (i in data) {
+    if (!data[i].Display || data[i].Display.toLowerCase() !== 'y') {
+      continue;
+    }
+    
+    $('body').append('<h4>' + data[i].Timestamp.split(' ')[0] + '</h4>');
+
     var keys = Object.keys(data[i]);
+
     for (j in keys) {
       var k = keys[j];
+      if (skipColumns.indexOf(k) > -1) continue;
+
       $('body').append('<h4>' + k + '</h4>');
       $('body').append('<div>' + data[i][k] + '</div>');
     }
-    $('body').append('<hr>')
+
+    $('body').append('<hr>');
   }
 }
