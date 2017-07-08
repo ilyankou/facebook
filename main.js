b11c62b82ca4fab0a07e69d8b20e489688b0f6c5
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-07-08 15:23:38 -0400

    Try accessing comments

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$.getJSON('https://graph.facebook.com/TrinityCollege/feed?access_token=2117684761791935|802f059657c6aeabc453b2ba31854625', function(data) {
+var accessToken = '2117684761791935|802f059657c6aeabc453b2ba31854625';
+
+$.getJSON('https://graph.facebook.com/TrinityCollege/feed?access_token=' + accessToken, function(data) {
   console.log(data);
+
+  for (i in data) {
+    var id = data[i].id;
+    $.getJSON('https://graph.facebook.com/' + id + '/comments?accessToken=' + accessToken, function(data) {
+      console.log(data);
+    });
+  }
+  
 });
