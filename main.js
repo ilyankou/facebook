204fd8930277e8f7e9f7872d1d4f09f0e28cb779
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-07-09 13:28:44 -0400

    Check pagination and load all comments"

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var pageID = 'TrinityCollege';
 var accessToken = '2117684761791935|802f059657c6aeabc453b2ba31854625';
 
-function addComments(id) {
+function loadComments(id, url=false) {
   $.ajax({
-    url: 'https://graph.facebook.com/{0}/comments?access_token={1}'.format(id, accessToken),
+    url: url ? url : 'https://graph.facebook.com/{0}/comments?access_token={1}'.format(id, accessToken),
     success: function(json) {
       comments = json.data;
+
       if (comments.length == 0) return;
 
       for (i in comments) {
-        console.log(comments[i])
         $('ul.' + id).append(
           '<li> \
             <i>{1} ({2}) on {0}:</i> <br> \
@@ -17,7 +17,13 @@ function addComments(id) {
             <ul class="{4}"></ul> \
           </li>'.format(moment(comments[i].created_time).format('LLL'), comments[i].from.name, comments[i].from.id, comments[i].message, comments[i].id));
 
-        addComments(comments[i].id);
+        // Load comments of the current comment
+        loadComments(comments[i].id);
+      }
+
+      // Facebook returns 25 comments by default. Check if more exist and load the next comments page
+      if (json.paging && json.paging.next) {
+        loadComments(id, json.paging.next);
       }
     }
   });
@@ -27,7 +33,6 @@ $.ajax({
   url: 'https://graph.facebook.com/{0}/feed?access_token={1}'.format(pageID, accessToken),
   success: function(json) {
     var posts = json.data;
-    console.log(posts);
 
     for (i in posts) {
       $('#posts').append(
@@ -38,6 +43,6 @@ $.ajax({
         </li>'
         .format(moment(posts[i].created_time).format('LLL'), posts[i].message, posts[i].id));
 
-      addComments(posts[i].id);
+      loadComments(posts[i].id);
     }
 }});
